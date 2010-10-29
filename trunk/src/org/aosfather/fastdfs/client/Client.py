2765e30870a67063e618a1ebe73fe7445ca5c135
faye.feelcool <faye.feelcool@3873e6bf-5f1c-977b-f271-62207f1065df>
2010-10-29 04:04:55 +0000

    初始化提交

---
+++ trunk/src/org/aosfather/fastdfs/client/Client.py
@@ -0,0 +1,18 @@
+'''
+Created on 2010-10-25
+The client class for the fastdfs.
+it;s the way of access fastdfs server.
+@author: xiaopengxiong
+'''
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+    def loadconfig(self):
+        '''
+         load the config info of the server .
+        '''    
---
+++ trunk/src/org/aosfather/fastdfs/client/ProtocolUtil.py
@@ -0,0 +1,6 @@
+'''
+Created on 2010-10-25
+the protocol of the fastdfs.
+the fastdfs protocol depend on tcpip
+@author: xiaopengxiong
+'''
