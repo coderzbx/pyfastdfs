cee1dec9be2ab0c25cf5a68aec5e4373ef3ad002
faye.feelcool@gmail.com <faye.feelcool@gmail.com@3873e6bf-5f1c-977b-f271-62207f1065df>
2011-11-25 03:28:40 +0000

    重新开发

--- trunk/src/org/aosfather/fastdfs/client/Protocol.py
+++
@@ -1,5 +0,0 @@
-'''
-Created on 2010-10-21
-the fastdfs protocol python implement.
-@author: xxp
-'''
--- trunk/src/org/aosfather/fastdfs/client/ProtocolUtil.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on 2010-10-25
-the protocol of the fastdfs.
-the fastdfs protocol depend on tcpip
-@author: xiaopengxiong
-'''
